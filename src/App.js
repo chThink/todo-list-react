de820205646c6fd207f9f5cd4092d016a1f9be74
chThink <champgn@icloud.com>
2024-10-10 17:52:47 -0700

    new: implementing basic logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import AddTask from "./AddTask";
+import TaskList from "./TaskList.js";
+
+const initialTasks = [
+  { id: 0, text: "Visit Kafka Museum", done: true },
+  { id: 1, text: "Watch a puppet show", done: false },
+  { id: 2, text: "Lennon Wall pic", done: false },
+];
 
 function App() {
+  const [tasks, setTasks] = useState(initialTasks || []);
+
+  function handleAddTask(text) {
+    setTasks([
+      ...tasks,
+      {
+        id: Date.now(),
+        text: text,
+        done: false,
+      },
+    ]);
+
+    console.log(tasks);
+  }
+
+  function handleChangeTask(task) {
+    setTasks(
+      tasks.map((t) => {
+        if (t.id === task.id) {
+          return task;
+        } else {
+          return t;
+        }
+      })
+    );
+  }
+
+  function handleDeleteTask(taskId) {
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Prague Itinerary</h1>
+      {/* add task */}
+
+      <AddTask onAddTask={handleAddTask} />
+      {/* list tasks */}
+
+      {tasks ? (
+        <TaskList
+          tasks={tasks}
+          onChangeTask={handleChangeTask}
+          onDeleteTask={handleDeleteTask}
+        />
+      ) : (
+        <>
+          <p>Nenhuma tarefa disponível {}</p>
+        </>
+      )}
     </div>
   );
 }
